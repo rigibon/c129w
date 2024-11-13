05efb785f0da71fefe34a37a87dc406c22de1ea4
rigibon <kenugba@outlook.com>
2024-11-13 11:33:59 -0300

    PORT

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { json, urlencoded } from 'body-parser';
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 declare const module: any;
 
@@ -11,7 +14,7 @@ async function bootstrap() {
   app.use(json({ limit: '10mb' }));
   app.use(urlencoded({ limit: '10mb', extended: true }));
 
-  await app.listen(3001);
+  await app.listen(process.env.PORT);
 
   if (module.hot) {
     module.hot.accept();
